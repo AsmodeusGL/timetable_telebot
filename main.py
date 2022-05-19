b689f0379c3273583adf420b588e556a892dad94
asmodeusgl <p.kruglikovskii@gmail.com>
2022-05-19 12:37:19 +0300

    created time func

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import name_token
 import telebot
+import datetime
 
 bot = telebot.TeleBot(name_token.name)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler()
 def start(message):
-    bot.send_message(message.chat.id, 'Привет!')
-
-
-@bot.message_handler(commands=['end'])
-def start(message):
-    bot.send_message(message.chat.id, 'Пока!')
+    match message.text.lower():
+        case 'время':
+            bot.send_message(message.chat.id, f'Время отправки сообщения {datetime.datetime.utcfromtimestamp(message.date + 3 * 60 * 60)}')
+        case _:
+            bot.reply_to(message, message.text)
 
 
 bot.polling()
