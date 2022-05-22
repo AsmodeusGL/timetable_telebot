4d781e469660f5acdab7883d502ad41280962f35
asmodeusgl <p.kruglikovskii@gmail.com>
2022-05-22 20:03:26 +0300

    created parser

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import name_token
+import parse
+
 import telebot
-import datetime
 from telebot import types
 
 bot = telebot.TeleBot(name_token.name)
 
 
-@bot.message_handler()
+@bot.message_handler(commands=['start'])
 def start(message):
-    match message.text.lower():
-        case 'расписание':
-            kb = types.InlineKeyboardMarkup(row_width=1)
-            button_bus = types.InlineKeyboardButton(text='Автобус', url='https://kogda.by/routes/minsk/autobus')
+    if message.text.isalpha():
+        if message.text.lower() == 'расписание':
+            keyboard = types.InlineKeyboardMarkup(row_width=1)
+            button_bus = types.InlineKeyboardButton(text='Автобус', callback_data='Автобус')
             button_trolleybus = types.InlineKeyboardButton(text='Троллейбус', url='https://kogda.by/routes/minsk/trolleybus')
             button_tram = types.InlineKeyboardButton(text='Трамвай', url='https://kogda.by/routes/minsk/tram')
             button_subway = types.InlineKeyboardButton(text='Метро', url='https://kogda.by/routes/minsk/metro')
-            kb.add(button_bus, button_trolleybus, button_tram, button_subway)
-            bot.send_message(message.chat.id, 'Расписание', reply_markup=kb)
-        case 'время':
-            bot.send_message(message.chat.id, f'Время отправки сообщения {datetime.datetime.utcfromtimestamp(message.date + 3 * 60 * 60)}')
-        case _:
-            bot.reply_to(message, message.text)
+            keyboard.add(button_bus, button_trolleybus, button_tram, button_subway)
+            bot.send_message(message.chat.id, 'Выберете тип транспорта', reply_markup=keyboard)
+        else:
+            bot.send_message(message.chat.id, 'Данный бот предназначен для получения информации расписания'
+                                              ' маршрутного транспортного средства. Введите "Расписание"')
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data)
+def check(callback):
+    if callback.data == 'Автобус':
+        bot.send_message(callback.message.chat.id, 'Введите номер автобуса')
+
+
+@bot.message_handler(func=lambda message: message.text.isdigit())
+def qwer(message):
+    x = parse.start()
+    bot.send_message(message.chat.id, f'{x[int(message.text) - 1]}')
+
 
 
 bot.polling()
---
+++ parse.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+def start():
+    arr = []
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('https://minsk.btrans.by/avtobus')
+    hrefs = driver.find_element(By.CLASS_NAME, 'hexagon-container')
+    links = hrefs.find_elements(By.TAG_NAME, 'a')
+    for el in links:
+        arr.append(el.get_attribute('href'))
+    return arr[:3]
