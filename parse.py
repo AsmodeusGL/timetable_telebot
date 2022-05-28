1ec8eb6e1a29379f4b91b7b79e017e77654346df
asmodeusgl <p.kruglikovskii@gmail.com>
2022-05-29 00:24:04 +0300

    timetable list

--- parse.py
+++ parse.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -17,11 +19,12 @@ def parse_numbers(url):
 
 
 def routes(url):
+    dictionary = {}
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
-    hrefs = driver.find_elements(By.CLASS_NAME, 'direction')
-    first = hrefs[0].find_element(By.TAG_NAME, 'h2').text
-    second = hrefs[1].find_element(By.TAG_NAME, 'h2').text
-    return [first, second]
+    time.sleep(2)
+    for number in range(2):
+        dictionary[driver.find_elements(By.CLASS_NAME, 'direction')[number].find_element(By.TAG_NAME, 'h2').text] = [item.text for item in driver.find_elements(By.CLASS_NAME, 'stops')[number].find_elements(By.CLASS_NAME, 'stop')]
+    return dictionary
